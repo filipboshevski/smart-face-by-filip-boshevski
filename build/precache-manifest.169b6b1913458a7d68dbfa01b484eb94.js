f505d37d027a89b77ef9a7644f1f0369f79de867
filipboshevski <filipbosevski1234@gmail.com>
2020-07-30 17:04:32 +0200

    Build

---
+++ build/precache-manifest.169b6b1913458a7d68dbfa01b484eb94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cb5317dda51abea95ada3216c623ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bae871eb88416dd323",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "845c283ebe6a6fc0d2ca",
+    "url": "/static/css/main.3bbe8368.chunk.css"
+  },
+  {
+    "revision": "08bae871eb88416dd323",
+    "url": "/static/js/2.8612a597.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.8612a597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845c283ebe6a6fc0d2ca",
+    "url": "/static/js/main.578a1ee5.chunk.js"
+  },
+  {
+    "revision": "8fae2f0bf44244f6ee82",
+    "url": "/static/js/runtime-main.14ada470.js"
+  },
+  {
+    "revision": "c555da565e9235e2e28ea78dd9926593",
+    "url": "/static/media/All The Roll - Personal Use.c555da56.otf"
+  },
+  {
+    "revision": "0dd2024bc0d74470849532878643b74e",
+    "url": "/static/media/brain.0dd2024b.png"
+  }
+]);
